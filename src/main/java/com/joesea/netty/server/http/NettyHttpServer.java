208f8c2abb293e98719e05821dfb72ce9cf76deb
JoeseaLea <joesealea@JoeseadeMacBook-Pro.local>
2020-02-21 13:31:02 +0800

    Http服务端编写

--- src/main/java/com/joesea/netty/server/http/NettyHttpServer.java
+++ src/main/java/com/joesea/netty/server/http/NettyHttpServer.java
@@ -1,9 +1,70 @@
 package com.joesea.netty.server.http;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
 /**
  * <p>@author : Joesea Lea</p>
  * <p>@date : 2020/2/21</p>
  * <p>@description : </p>
  */
 public class NettyHttpServer {
+
+    private static final int PORT = 8001;
+
+    private static final int BIZGROUPSIZE = Runtime.getRuntime().availableProcessors()*2;
+    private static final int BIZTHREADSIZE = 100;
+    private static final EventLoopGroup bossGroup = new NioEventLoopGroup(BIZGROUPSIZE);
+    private static final EventLoopGroup workerGroup = new NioEventLoopGroup(BIZTHREADSIZE);
+
+    public static void service() throws Exception {
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup,workerGroup)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel> () {
+
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+//                        ChannelPipeline pipeline = socketChannel.pipeline();
+//                        pipeline.addLast(new HttpServerCodec());// http 编解码
+//                        pipeline.addLast("httpAggregator",new HttpObjectAggregator(512*1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
+//                        pipeline.addLast(new NettyHttpServerHandler());// 请求处理器
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new HttpServerCodec());// http 编解码
+//                        pipeline.addLast("encoder",new HttpResponseEncoder());
+//                        pipeline.addLast("decoder",new HttpRequestDecoder());
+                        pipeline.addLast(new HttpObjectAggregator(10*1024*1024));//把单个http请求转为FullHttpReuest或FullHttpResponse
+                        pipeline.addLast(new NettyHttpServerHandler());// 服务端业务逻辑
+                    }
+                });
+
+        ChannelFuture f = bootstrap.bind(new InetSocketAddress(PORT)).sync();
+        System.out.println("Http服务器启动完成");
+        f.channel().closeFuture().sync();
+
+    }
+
+    protected static void shutdown() {
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("开始启动Http服务器...");
+        NettyHttpServer.service();
+    }
 }
--- src/main/java/com/joesea/netty/server/http/NettyHttpServerHandler.java
+++ src/main/java/com/joesea/netty/server/http/NettyHttpServerHandler.java
@@ -1,9 +1,160 @@
 package com.joesea.netty.server.http;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * <p>@author : Joesea Lea</p>
  * <p>@date : 2020/2/21</p>
  * <p>@description : </p>
  */
-public class NettyHttpServerHandler {
+public class NettyHttpServerHandler extends ChannelInboundHandlerAdapter {
+//    private ServerBean serverBean;
+//    private ConcurrentHashMap<String, MapData> channelMap;
+    private HttpRequest request = null;
+    private int contentLen = 0;
+    private ByteBuf msgBuf = null;
+    private String uuid = null;
+//    public ChannelBuffer bufferOK = null;
+//    public ChannelBuffer bufferError = null;
+    public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private String result="";
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        //请求头处理
+//        if (msg instanceof HttpRequest) {
+//            request = (HttpRequest) msg;
+//            //获取content内容的长度
+//            String str = request.headers().get("Content-Length");
+//            if (str != null) {
+//                contentLen = Integer.parseInt(request.headers().get("Content-Length"));
+//                if(msgBuf == null){
+//                    msgBuf = Unpooled.buffer(contentLen);
+//                }
+//            }
+//        }
+//        //请求内容获取
+//        if (msg instanceof HttpContent) {
+//            HttpContent content = (HttpContent) msg;
+//            ByteBuf buf = content.content();
+////            msgBuf.writeBytes(buf);
+////            buf.release();
+//            System.out.println(buf.toString(CharsetUtil.UTF_8));
+//        }
+
+        if(!(msg instanceof FullHttpRequest)){
+            result="未知请求!";
+            send(ctx,result,HttpResponseStatus.BAD_REQUEST);
+            return;
+        }
+        FullHttpRequest httpRequest = (FullHttpRequest) msg;
+        try{
+            String path=httpRequest.uri();          //获取路径
+            String body = getBody(httpRequest);     //获取参数
+            HttpMethod method=httpRequest.method();//获取请求方法
+            //如果不是这个路径，就直接返回错误
+//            if(!"/test".equalsIgnoreCase(path)){
+//                result="非法请求!";
+//                send(ctx,result,HttpResponseStatus.BAD_REQUEST);
+//                return;
+//            }
+            System.out.println("接收到:"+method+" 请求");
+            //如果是GET请求
+            if(HttpMethod.GET.equals(method)){
+                //接受到的消息，做业务逻辑处理...
+                System.out.println("body:"+body);
+                result="GET请求";
+                send(ctx,result,HttpResponseStatus.OK);
+                return;
+            }
+            //如果是POST请求
+            if(HttpMethod.POST.equals(method)){
+                //接受到的消息，做业务逻辑处理...
+                System.out.println("body:"+body);
+                result="POST请求";
+                send(ctx,result,HttpResponseStatus.OK);
+                return;
+            }
+
+            //如果是PUT请求
+            if(HttpMethod.PUT.equals(method)){
+                //接受到的消息，做业务逻辑处理...
+                System.out.println("body:"+body);
+                result="PUT请求";
+                send(ctx,result,HttpResponseStatus.OK);
+                return;
+            }
+            //如果是DELETE请求
+            if(HttpMethod.DELETE.equals(method)){
+                //接受到的消息，做业务逻辑处理...
+                System.out.println("body:"+body);
+                result="DELETE请求";
+                send(ctx,result,HttpResponseStatus.OK);
+                return;
+            }
+            //如果是DELETE请求
+            if(HttpMethod.OPTIONS.equals(method)){
+                //接受到的消息，做业务逻辑处理...
+                System.out.println("body:"+body);
+                result="OPTIONS请求";
+                send(ctx,result,HttpResponseStatus.OK);
+                return;
+            }
+        }catch(Exception e){
+            System.out.println("处理请求失败!");
+            e.printStackTrace();
+        }finally{
+            //释放请求
+            httpRequest.release();
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+
+    /**
+     * 发送的返回值
+     * @param ctx     返回
+     * @param context 消息
+     * @param status 状态
+     */
+    private void send(ChannelHandlerContext ctx, String context, HttpResponseStatus status) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status, Unpooled.copiedBuffer(context, CharsetUtil.UTF_8));
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 获取body参数
+     * @param request
+     * @return
+     */
+    private String getBody(FullHttpRequest request){
+        ByteBuf buf = request.content();
+        return buf.toString(CharsetUtil.UTF_8);
+    }
+
+
 }
